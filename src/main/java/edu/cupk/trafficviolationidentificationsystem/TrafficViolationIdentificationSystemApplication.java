60f86340ffecbea685e148b53441cf7b58b1ceda
恬风 <rainkaze@qq.com>
2025-06-12 19:16:53 +0800

    🎉 创建仓库，初次提交

---
+++ src/main/java/edu/cupk/trafficviolationidentificationsystem/TrafficViolationIdentificationSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.cupk.trafficviolationidentificationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficViolationIdentificationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficViolationIdentificationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/cupk/trafficviolationidentificationsystem/TrafficViolationIdentificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cupk.trafficviolationidentificationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficViolationIdentificationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
