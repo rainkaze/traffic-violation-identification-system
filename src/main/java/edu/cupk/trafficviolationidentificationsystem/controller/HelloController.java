77d879207aceb6218a22d9ad3d95c6b8a933fd0f
恬风 <rainkaze@qq.com>
2025-06-12 20:48:51 +0800

    ✨ 初次测试前后端数据交互

--- src/main/java/edu/cupk/trafficviolationidentificationsystem/controller/HelloController.java
+++ src/main/java/edu/cupk/trafficviolationidentificationsystem/controller/HelloController.java
@@ -1,9 +1,11 @@
 package edu.cupk.trafficviolationidentificationsystem.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin // 允许所有来源的跨域请求
 public class HelloController {
 
     @RequestMapping("/hello")
