fef4d62a4dd4e988890b1705fe6684b488abf3eb
zxq <3235358845@qq.com>
2025-06-13 01:26:14 +0800

    🎉 zxq 初次提交进行测试

--- src/main/java/edu/cupk/trafficviolationidentificationsystem/controller/HelloController.java
+++ src/main/java/edu/cupk/trafficviolationidentificationsystem/controller/HelloController.java
@@ -1,5 +1,4 @@
 package edu.cupk.trafficviolationidentificationsystem.controller;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 //@CrossOrigin // 允许所有来源的跨域请求
 public class HelloController {
+
 
     @RequestMapping("/hello")
     public String hello() {
