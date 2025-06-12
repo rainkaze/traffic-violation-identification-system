44c7ea4e82956dc59b21f37016eb61d136cfbe33
zxq <3235358845@qq.com>
2025-06-13 01:48:12 +0800

    ✨ zxq 统计分析模块：测试后端向前端发送模拟数据成功

--- src/main/java/edu/cupk/trafficviolationidentificationsystem/controller/HelloController.java
+++ src/main/java/edu/cupk/trafficviolationidentificationsystem/controller/HelloController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 //@CrossOrigin // 允许所有来源的跨域请求
 public class HelloController {
-
-
     @RequestMapping("/hello")
     public String hello() {
         return "hello Spring Boot!";
