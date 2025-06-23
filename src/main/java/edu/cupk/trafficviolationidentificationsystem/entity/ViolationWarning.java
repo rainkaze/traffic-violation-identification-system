0533e893dad4c4a75673eae65d12844da74e1970
yr_123 <3175352680@qq.com>
2025-06-23 12:33:39 +0800

    ✨写了函数对违法记录的违法类型判断插入对应的预警关联表

--- src/main/java/edu/cupk/trafficviolationidentificationsystem/entity/ViolationWarning.java
+++ src/main/java/edu/cupk/trafficviolationidentificationsystem/entity/ViolationWarning.java
@@ -1,7 +1,16 @@
 package edu.cupk.trafficviolationidentificationsystem.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Timestamp;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ViolationWarning {
     private Long id;
     private Long violationId;
